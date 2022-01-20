f736106a54b95c54a83c7b81ce351691409e98b6
katarinasokcevic <katarina.sokcevic.vk@gmail.com>
2022-01-20 13:37:48 +0100

    initial code

---
+++ Lyrix/app/src/main/java/com/example/lyrix/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.lyrix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Handler().postDelayed(Runnable {
+            val i = Intent(this@MainActivity, BrowseActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 1000)
+    }
+}
